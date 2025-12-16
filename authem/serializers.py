480b3ca1087c6f510db8072f062a2adcb35f2f4c
Cipo <cipokibona.ck@gmail.com>
2025-12-16 16:17:12 +0200

    update userserializer: ajout de password et is_active dans fields

--- authem/serializers.py
+++ authem/serializers.py
@@ -3,10 +3,20 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'password', 'email', 'role', 'is_active']
 
+        def update(self, instance, validated_data):
+
+            if "password" in validated_data:
+                password = validated_data.pop("password")
+                if password:
+                    instance.set_password(password)
+                
+                return super().update(instance, validated_data)
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
